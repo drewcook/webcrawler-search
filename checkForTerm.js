43ac0cb5355d9dc39252885e2af7cd3a5e2fe70b
Drew Cook <drewcook29@gmail.com>
2021-08-02 12:36:34 -0600

    recurse

--- checkForTerm.js
+++ checkForTerm.js
@@ -14,6 +14,9 @@ const checkForTerm = (document, term) => {
 	// Traversal - we're only choosing to parse what is in the <body> tag to speed up the process
 	const body = document.body
 
+	const recursionResult = recurse(body, results)
+	console.log(recursionResult)
+
 	// TODO: UGH - too much time using a bad URL to test my JSDOM parsing resulted in me not finishing the traversal part
 	// for (const child of body.children) {
 	// 	if (child.children.length)
@@ -36,12 +39,12 @@ const checkForTerm = (document, term) => {
 // TODO: use recursion to traverse and compile all the HTML inner texts into an array
 // TODO: loop over array of text contents and count number of times 'TERM' is substring
 // TODO: return number of instances found on a page along with the substring of preceding and following words around the term - "word TERM word"
-const recurse = domSlice => {
+const recurse = (domSlice, results) => {
 	if (domSlice.children.length === 0) {
 		return domSlice.textContent
 	} else {
 		for (const child of domSlice.children) {
-			return recurse(child) + recurse(child.children)
+			return results.push(recurse(child))
 		}
 	}
 }
