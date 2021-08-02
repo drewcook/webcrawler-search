2aba809f9a3d10f19569369b6474ece6df501845
Drew Cook <drewcook29@gmail.com>
2021-08-02 13:00:41 -0600

    cleanup

--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
 const { JSDOM } = require('jsdom')
 
+// TODO: loop over array of text contents and count number of times 'TERM' is substring
+// TODO: return number of instances found on a page along with the substring of preceding and following words around the term - "word TERM word"
+
 const parseHTMLPage = (pageData, url) => {
 	const { document } = new JSDOM(pageData, {
 		url,
@@ -9,45 +12,34 @@ const parseHTMLPage = (pageData, url) => {
 }
 
 const checkForTerm = (document, term) => {
-	let results = []
-	let texts = ['mock', 'data', 'apple', 'reimagined']
+	let mocks = ['mock', 'data', 'apple', 'reimagined']
 	// Traversal - we're only choosing to parse what is in the <body> tag to speed up the process
-	const body = document.body
+	const results = compilePageText(document.body)
+	console.log(results)
 
-	// const recursionResult = recurse(body, results)
-	// console.log(recursionResult)
-
-	// TODO: UGH - too much time using a bad URL to test my JSDOM parsing resulted in me not finishing the traversal part
-	// for (const child of body.children) {
-	// 	if (child.children.length)
-	// }
-	// for (let i = 0; i < body.childNodes.length; i++) {
-	// 	const node = body.childNodes[i]
-	// 	console.log(node)
-	// 	if (node.children.length === 0) {
-	// 		console.log(node.textContent)
-	// 	} else {
-	// 		for (const child of node.childNodes) {
-	// 			console.log(child)
-	// 		}
-	// 	}
-	// }
-
-	return texts
+	// return results
+	return mocks
 }
 
-// TODO: use recursion to traverse and compile all the HTML inner texts into an array
-// TODO: loop over array of text contents and count number of times 'TERM' is substring
-// TODO: return number of instances found on a page along with the substring of preceding and following words around the term - "word TERM word"
-const recurse = (domSlice, results) => {
-	if (domSlice.children.length === 0) {
-		return domSlice.textContent
-	} else {
-		for (const child of domSlice.children) {
-			console.log(child.children.length)
-			// return results.push(recurse(child))
-		}
+// Use recursion to traverse and compile all the HTML inner texts into an array
+const compilePageText = pageDom => {
+	let results = []
+
+	const nodes = body.getElementsByTagName('*')
+
+	for (var i = 0; i < domNodes.length; i++) {
+		console.log(nodes[i].textContent)
 	}
+	function helper(input) {
+		if (input.length === 0) {
+			return input.textContent
+		}
+		return input.textContent + helper(input.children)
+	}
+
+	helper(domNodes)
+
+	return results
 }
 
 module.exports = {
