d24a32cc7c840824dbbb4a3f3c99ac978f7109f8
Drew Cook <drewcook29@gmail.com>
2021-08-02 13:49:49 -0600

    cleanup

--- utils.js
+++ utils.js
@@ -1,8 +1,5 @@
 const { JSDOM } = require('jsdom')
 
-// TODO: loop over array of text contents and count number of times 'TERM' is substring
-// TODO: return number of instances found on a page along with the substring of preceding and following words around the term - "word TERM word"
-
 const parseHTMLPage = (pageData, url) => {
 	const { document } = new JSDOM(pageData, {
 		url,
@@ -13,50 +10,29 @@ const parseHTMLPage = (pageData, url) => {
 
 // Use recursion to traverse and compile all the HTML inner texts into an array
 const compilePageText = pageDom => {
-	let results = []
-	const nodes = pageDom.getElementsByTagName('*')
-
-	// for (var i = 0; i < nodes.length; i++) {
-	// 	const text = nodes[i].textContent
-	// 	if (text.length > 0) results.push(text)
-	// }
-
-	function helper(input) {
-		console.log(input.children)
-		if (input.children.length === 0) {
-			console.log('no child')
-			results.push(input.textContent.replace(/[\n\r\t]/g, '').trim())
-		} else {
-			console.log(input.children)
-			// input.children.forEach(child => helper(child))
-			// helper(input.children)
-		}
-	}
-
-	helper(pageDom)
-
-	return results
+	let text = pageDom.textContent?.replace(/[\n\r\t]/g, '').trim() || ''
+	if (!pageDom.children) return text
+	text = text.concat(compilePageText(pageDom.children))
+	return text.split(' ').filter(word => word !== '')
 }
 
 const checkForTerm = (document, term) => {
 	// Traversal - we're only choosing to parse what is in the <body> tag to speed up the process
-	const texts = compilePageText(document.body)
-	console.log(texts)
+	const pageText = compilePageText(document.body)
 
 	// Compile output for user on all occurrances found for 'term'
 	let occurrances = 0
 	let results = []
-
-	for (let i = 0; i < texts.length; i++) {
-		const substr = results[i]
-		if (substr.includes(term)) {
-			count++
-			results.push(substr)
+	for (let i = 0; i < pageText.length; i++) {
+		const word = pageText[i]
+		if (word.toLowerCase() === term.toLowerCase()) {
+			occurrances++
+			results.push(`${pageText[i - 1]} ${word} ${pageText[i + 1]}`)
 		}
 	}
 
 	// Output to user
-	console.log(`Found the term ${term} a total of ${occurrances} times.`)
+	console.log(`Found the term "${term}" a total of ${occurrances} times.`)
 	for (const result of results) {
 		console.log(`Occurrance found: ${result}`)
 	}
